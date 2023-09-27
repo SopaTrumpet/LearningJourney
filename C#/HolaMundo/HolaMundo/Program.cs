81474a79f74ad60e4b238502b992a6c2d1c73a86
SopaTrumpet <77031337+SopaTrumpet@users.noreply.github.com>
2023-09-27 00:00:06 -0400

    Add files via upload

---
+++ C#/HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo desde C#");
+        Console.WriteLine("Esta es otra linea de texto");
+    }
+}
---
+++ C#/Variables/Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero = 10;
+        int resultado = numero * 5;
+        Console.WriteLine("{0}",resultado);
+
+        int suma = 8 + 20;
+        Console.WriteLine(suma);
+
+        String cadena = "Amo mucho a mi novia bella";
+        Console.WriteLine(cadena);
+
+    }
+}
